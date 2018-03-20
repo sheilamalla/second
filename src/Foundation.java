a73d09ddd932a8d6003f76f45b62bb4b473dff69
mallasheela@yahoo.com <mallasheela@yahoo.com>
2018-03-20 14:24:01 -0400

    test

---
+++ src/Foundation.java
@@ -0,0 +1,15 @@
+
+
+
+
+interface Automobile { String describe(); }
+
+class FourWheeler implements Automobile{
+   String name;
+   public String describe(){ return " 4 Wheeler " + name; }
+}
+
+class TwoWheeler extends FourWheeler{
+    String name;
+    public String describe(){ return " 2 Wheeler " + name; }
+}
